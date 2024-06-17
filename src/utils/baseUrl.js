7ee245ac07680da44eb29a917b8719290c7b4557
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-06-17 19:35:03 +0530

    deployment check v2

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,4 @@
 export const baseUrl =
 	process.env.NODE_ENV === 'production'
-		? "server.com/"
+		? 'https://pizza-wizza-next-aguhvpj08-dhaneshwar7s-projects.vercel.app/'
 		: 'http://localhost:3000/';
